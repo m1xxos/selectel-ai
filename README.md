1bb052ed46b5a6296f06f21d97c944dc7ed97d6a
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-03-03 20:32:27 +0300

    feat:project start

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# yc-ai
+<h1 align="center">
+  <br>
+  <a href="https://github.com/m1xxos/homelab">
+    <img src="assets/icon.png" alt="Logo" width="80" height="80">
+  </a>
+  <br>
+  yc-ai
+  <br>
+</h1>
+
+<h4 align="center">Ai with Yandex Cloud k8s gpus</h4>
+
+## How to use
+Steps to get started
+
+1. Generate new logo at https://ray.so/icon
+1. Add values to terraform/yc.tfstate
+2. Init terraform state with command
+
+```bash
+terraform init -backend-config=yc.auto.tfvars
+```
---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    endpoint                    = "https://storage.yandexcloud.net"
+    region                      = "ru-central1"
+    bucket                      = var.bucket
+    key                         = "yc-ai.tfstate"
+    access_key                  = var.access_key
+    secret_key                  = var.secret_key
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
